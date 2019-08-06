c0b5b79b1d07543e72c45cd30b00f1c5ccc17269
Ben Gohlke <jcgohlke@me.com>
2019-08-06 19:32:42 -0400

    Initial commit

---
+++ TurkeyCalculator/TurkeyCalculator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TurkeyCalculator
+//
+//  Created by Paul Solt on 6/3/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Outlets
+    
+    @IBOutlet var turkeyWeightTextField: UITextField!
+    @IBOutlet var cookTimeTextField: UITextField!
+    @IBOutlet var metricButton: UIButton!
+    
+    // Actions
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func metricButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    // Helper functions
+}
